08ddadfc8a2dd369880e90432ec5a883cc1ad274
rishav chhajer <rishavchhajer@gmail.com>
2023-11-17 14:16:53 +0530

    add monitor route

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
+app.get("/monitor", (req, res) => res.status(200).json({success: "ok"}));
+
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
